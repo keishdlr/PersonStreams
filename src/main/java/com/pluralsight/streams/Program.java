b5c3c6555b963085171461d4ccbe71445870e3de
keisha De La Rosa <keishdlr@gmail.com>
2025-11-05 15:39:13 -0500

    add a list of 10 people

--- src/main/java/com/pluralsight/streams/Program.java
+++ src/main/java/com/pluralsight/streams/Program.java
@@ -1,7 +1,23 @@
 package com.pluralsight.streams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
+        List<Person> person = new ArrayList<>();
+
+        person.add(new Person("Maya", "Thompson", 28));
+        person.add(new Person("Leo", "Ramirez", 34));
+        person.add(new Person("Aisha", "Patel", 22));
+        person.add(new Person("Marcus", "Nguyen", 41));
+        person.add(new Person("Zoe", "Kim", 19));
+        person.add(new Person("Joseph", "Brooks", 37));
+        person.add(new Person("Clara", "Moreno", 26));
+        person.add(new Person("Jamal", "Carter", 45));
+        person.add(new Person("Lily", "Chen", 31));
+        person.add(new Person("Andre", "Sullivan", 52));
+
 
     }
 }
